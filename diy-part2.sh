3c0a0a791ef7a793d2d93cbe97cd02659c9ccbf2
storybin <storynew@163.com>
2025-10-14 23:26:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,9 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 # Modify hostname
 sed -i 's/LEDE/NST/g' package/base-files/files/bin/config_generate
+
+# 清除默认密码，这使得首次登录时无需密码
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
+
+# 修改默认密码
+sed -i 's/root::0:0:99999:7:::/root:$1$storybin$ObUyPWTLM/e4QZjTAYiSg1:18336:0:99999:7:::/g' package/base-files/files/etc/shadow
