965b1379a963a97c33ff3d25589236d2760140f9
storybin <storynew@163.com>
2025-10-18 03:06:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git openappfilter https://github.com/destan19/OpenAppFilter' >>feeds.conf.default
